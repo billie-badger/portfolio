1c5e0f700062dce9d623f9fb8f24b3a6e04ae949
billie-badger <will.vatcher@gmail.com>
2018-01-26 01:41:22 -0800

    Added codepen social and media query

--- src/components/SocialBar/SocialBar.js
+++ src/components/SocialBar/SocialBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SocialContainer, Github, Linkedin, Resume } from './SocialBar.style';
+import { SocialContainer, Github, Linkedin, Resume, Codepen } from './SocialBar.style';
 
 
 class SocialBar extends Component {
@@ -8,6 +8,7 @@ class SocialBar extends Component {
             <SocialContainer>
                 <a href='https://github.com/billie-badger' target='_blank'><Github /></a>
                 <a href='https://www.linkedin.com/in/will-vatcher-9705b340/' target='_blank'><Linkedin /></a>
+                <a href='https://codepen.io/willvatcher/' target='_blank'><Codepen /></a>
                 <a href ='https://github.com/billie-badger/Resume/blob/master/Will_Vatcher.pdf' target='_blank'><Resume /></a>
             </SocialContainer>
         );
--- src/components/SocialBar/SocialBar.style.js
+++ src/components/SocialBar/SocialBar.style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Flex, Div } from '../../themes/grid';
+import media from '../../themes/media';
 
 export const SocialContainer = styled(Flex)`
     position: fixed;
@@ -7,15 +8,25 @@ export const SocialContainer = styled(Flex)`
     bottom: 1em;
     width: 60px;
     margin-left: 1em;
-
     align-items: center;
-    z-index: 800;
+    z-index: 600;
+
+    ${media.tablet`
+        position: fixed;
+        
+        flex-direction: row;
+        margin-left: 0;
+        top: -70vh;
+        width: 100%;
+        justify-content: space-around;
+
+    `}
 `;
 
 export const Github = styled(Flex)`
     
-    height:50px;
-    width:50px;
+    height:40px;
+    width:40px;
     background-image: url('${require('../../assets/Social/github.png')}');
     background-repeat: no-repeat;
     background-size: cover;
@@ -26,9 +37,22 @@ export const Github = styled(Flex)`
 
 export const Linkedin = styled(Flex)`
 
-    height:50px;
-    width:50px;
+    height:40px;
+    width:40px;
     background-image: url('${require('../../assets/Social/linkedin.png')}');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    margin-bottom: 1em;
+    
+`;
+
+
+export const Codepen = styled(Flex)`
+
+    height:40px;
+    width:40px;
+    background-image: url('${require('../../assets/Social/codepen.png')}');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
@@ -38,8 +62,8 @@ export const Linkedin = styled(Flex)`
 
 export const Resume = styled(Flex)`
 
-    height:50px;
-    width:50px;
+    height:40px;
+    width:40px;
     background-image: url('${require('../../assets/Social/resume.png')}');
     background-repeat: no-repeat;
     background-size: cover;
